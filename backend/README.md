0b4fc387d1af7b5e3aead6f544ac3f7b7ff958d1
cawinchan <cawin.chan@gmail.com>
2022-05-14 22:29:31 +0800

    Improve IR camera 16hz frame rate

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,3 @@
-# capstone_software_subsystem
-Capstone Project S15 HTX: Software subsystem for Urban Search and Rescue. This comprises of a Robotic software system for the control of data and localisation, Lifeform Detection Algorithm and User frontend software.
+# Backend 
+
+This comprises of a Robotic software system for the control of data and localisation, Lifeform Detection Algorithm
--- backend/Sensorscripts/mlx90640_temp/MLX90640_temp.ino
+++ backend/Sensorscripts/mlx90640_temp/MLX90640_temp.ino
@@ -1,92 +1,110 @@
-#include <Adafruit_MLX90640.h>
+/*
+  Output the temperature readings to all pixels to be read by a Processing visualizer
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: May 22nd, 2018
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
 
-Adafruit_MLX90640 mlx;
-float frame[32*24]; // buffer for full frame of temperatures
+  Feel like supporting open source hardware?
+  Buy a board from SparkFun! https://www.sparkfun.com/products/14769
 
-// uncomment *one* of the below
-//#define PRINT_TEMPERATURES
-#define PRINT_ASCIIART
+  This example outputs 768 temperature values as fast as possible. Use this example
+  in conjunction with our Processing visualizer.
 
-void setup() {
-  while (!Serial) delay(10);
-  Serial.begin(115200);
-  delay(100);
+  This example will work with a Teensy 3.1 and above. The MLX90640 requires some
+  hefty calculations and larger arrays. You will need a microcontroller with 20,000
+  bytes or more of RAM.
 
-  Serial.println("Adafruit MLX90640 Simple Test");
-  if (! mlx.begin(MLX90640_I2CADDR_DEFAULT, &Wire)) {
-    Serial.println("MLX90640 not found!");
-    while (1) delay(10);
-  }
-  Serial.println("Found Adafruit MLX90640");
+  This relies on the driver written by Melexis and can be found at:
+  https://github.com/melexis/mlx90640-library
 
-  Serial.print("Serial number: ");
-  Serial.print(mlx.serialNumber[0], HEX);
-  Serial.print(mlx.serialNumber[1], HEX);
-  Serial.println(mlx.serialNumber[2], HEX);
+  Hardware Connections:
+  Connect the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
+  to the Qwiic board
+  Connect the male pins to the Teensy. The pinouts can be found here: https://www.pjrc.com/teensy/pinout.html
+  Open the serial monitor at 115200 baud to see the output
+*/
+
+#include <Wire.h>
+
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+
+const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
+
+#define TA_SHIFT 8 //Default shift for MLX90640 in open air
+
+float mlx90640To[768];
+paramsMLX90640 mlx90640;
+
+void setup()
+{
+  Wire.begin();
+  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+
+  Serial.begin(115200); //Fast serial as possible
   
-  //mlx.setMode(MLX90640_INTERLEAVED);
-  mlx.setMode(MLX90640_CHESS);
-  Serial.print("Current mode: ");
-  if (mlx.getMode() == MLX90640_CHESS) {
-    Serial.println("Chess");
-  } else {
-    Serial.println("Interleave");    
+  while (!Serial); //Wait for user to open terminal
+  //Serial.println("MLX90640 IR Array Example");
+
+  if (isConnected() == false)
+  {
+    Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
+    while (1);
   }
 
-  mlx.setResolution(MLX90640_ADC_18BIT);
-  Serial.print("Current resolution: ");
-  mlx90640_resolution_t res = mlx.getResolution();
-  switch (res) {
-    case MLX90640_ADC_16BIT: Serial.println("16 bit"); break;
-    case MLX90640_ADC_17BIT: Serial.println("17 bit"); break;
-    case MLX90640_ADC_18BIT: Serial.println("18 bit"); break;
-    case MLX90640_ADC_19BIT: Serial.println("19 bit"); break;
-  }
+  //Get device parameters - We only have to do this once
+  int status;
+  uint16_t eeMLX90640[832];
+  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+  if (status != 0)
+    Serial.println("Failed to load system parameters");
 
-  mlx.setRefreshRate(MLX90640_2_HZ);
-  Serial.print("Current frame rate: ");
-  mlx90640_refreshrate_t rate = mlx.getRefreshRate();
-  switch (rate) {
-    case MLX90640_0_5_HZ: Serial.println("0.5 Hz"); break;
-    case MLX90640_1_HZ: Serial.println("1 Hz"); break; 
-    case MLX90640_2_HZ: Serial.println("2 Hz"); break;
-    case MLX90640_4_HZ: Serial.println("4 Hz"); break;
-    case MLX90640_8_HZ: Serial.println("8 Hz"); break;
-    case MLX90640_16_HZ: Serial.println("16 Hz"); break;
-    case MLX90640_32_HZ: Serial.println("32 Hz"); break;
-    case MLX90640_64_HZ: Serial.println("64 Hz"); break;
-  }
+  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  if (status != 0)
+    Serial.println("Parameter extraction failed");
+
+  //Once params are extracted, we can release eeMLX90640 array
+
+  //MLX90640_SetRefreshRate(MLX90640_address, 0x02); //Set rate to 2Hz
+//  MLX90640_SetRefreshRate(MLX90640_address, 0x03); //Set rate to 4Hz
+//    MLX90640_SetRefreshRate(MLX90640_address, 0x04); //Set rate to 8Hz Most optimal speed to accuracy
+  MLX90640_SetRefreshRate(MLX90640_address, 0x05); //Set rate to 16Hz
 }
 
-void loop() {
-  delay(500);
-  if (mlx.getFrame(frame) != 0) {
-    Serial.println("Failed");
-    return;
+void loop()
+{
+  long startTime = millis();
+  for (byte x = 0 ; x < 2 ; x++)
+  {
+    uint16_t mlx90640Frame[834];
+    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+
+    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+
+    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float emissivity = 0.95;
+
+    MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
   }
-  Serial.println();
-  Serial.println();
-  for (uint8_t h=0; h<24; h++) {
-    for (uint8_t w=0; w<32; w++) {
-      float t = frame[h*32 + w];
-#ifdef PRINT_TEMPERATURES
-      Serial.print(t, 1);
-      Serial.print(", ");
-#endif
-#ifdef PRINT_ASCIIART
-      char c = '&';
-      if (t < 20) c = ' ';
-      else if (t < 23) c = '.';
-      else if (t < 25) c = '-';
-      else if (t < 27) c = '*';
-      else if (t < 29) c = '+';
-      else if (t < 31) c = 'x';
-      else if (t < 33) c = '%';
-      else if (t < 35) c = '#';
-      else if (t < 37) c = 'X';
-      Serial.print(c);
-#endif
-    }
-    Serial.println();
+  long stopTime = millis();
+
+  for (int x = 0 ; x < 768 ; x++)
+  {
+    //if(x % 8 == 0) Serial.println();
+    Serial.print(mlx90640To[x], 2);
+    Serial.print(",");
   }
+  Serial.println("");
+}
+
+//Returns true if the MLX90640 is detected on the I2C bus
+boolean isConnected()
+{
+  Wire.beginTransmission((uint8_t)MLX90640_address);
+  if (Wire.endTransmission() != 0)
+    return (false); //Sensor did not ACK
+  return (true);
 }
